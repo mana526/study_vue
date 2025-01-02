8e7b3a6504e5355b12c99045154fa14ce4fbca70
Imaoka Mana <imaaaana.26@gmail.com>
2025-01-02 23:21:50 +0900

    コンポーネント間の通信

---
+++ vue_comp/components-basics/src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import OneSection from './components/OneSection.vue'
+import OneInfo from "./components/OneInfo.vue";
+
+const rand = Math.floor(Math.random() * 100);
+const propsContent = ref(rand);
+</script>
+
+<template>
+  <div>
+    <h2>コンポーネントの独立性</h2>
+    <OneSection />
+    <OneSection />
+  </div>
+
+  <div>
+    <h2>親から子へのコンポーネント間通信</h2>
+    <p>Propsの基礎</p>
+    <OneInfo
+      title="propsの利用"
+      content="子コンポーネントにデータを渡すにはPropsを利用する。"
+      v-bind:props-content="propsContent"/>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ vue_comp/components-basics/src/components/OneInfo.vue
@@ -0,0 +1,17 @@
+ <script setup lang="ts">
+interface Props {
+  title: string;
+  content: string;
+  propsContent: number;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <section>
+    <h4>{{ title }}</h4>
+    <p>{{ content }}</p>
+    <p>{{ propsContent }}</p>
+  </section>
+</template>
