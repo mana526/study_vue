a504e26063354dc15493e39617d3e4bf96e29754
Imaoka Mana <imaaaana.26@gmail.com>
2025-01-05 18:44:18 +0900

    算出プロパティ

---
+++ vue_computed/computed/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref, computed } from "vue";
+
+const radiusInit = Math.floor(Math.random() * 10);
+const PI = ref(3.14);
+const radius = ref(radiusInit);
+// const areaInit = PI.value * radiusInit * radiusInit;
+// const area = ref(areaInit);
+
+const area = computed(
+  (): number => {
+    return radius.value * radius.value * PI.value;
+  }
+);
+</script>
+
+<template>
+  <p>半径{{ radius }}の円の面積を円周率{{ PI }}で計算すると、{{ area }}</p>
+</template>
+
+<style scoped>
+</style>
